1dafd43af9fbd7e3386726fd455464c33c62f316
Mira Hiltunen <mira@exove.fi>
2018-10-30 19:24:48 +0200

    Initial Commit

---
+++ Magic Mirror/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Mirror
+//
+//  Created by Hiltunen on 30/10/2018.
+//  Copyright © 2018 Hiltunen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
